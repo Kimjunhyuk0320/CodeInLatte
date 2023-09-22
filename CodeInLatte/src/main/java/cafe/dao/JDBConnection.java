c7d9a8e7793e9ab3d7287d5349b138c4704b5336
Kimjunhyuk0320 <kimminuk0320@gmail.com>
2023-09-22 18:53:59 +0900

    .gitignore - JDBConnection.java 무시

--- CodeInLatte/src/main/java/cafe/dao/JDBConnection.java
+++ CodeInLatte/src/main/java/cafe/dao/JDBConnection.java
@@ -16,7 +16,6 @@ public class JDBConnection {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");	 
-			
 			String url = "jdbc:mysql://localhost:3306/joeun?serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true&useSSL=false";
 			String id = "joeun";
 			String pw = "123456";
